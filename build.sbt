997cb8f0ed03858ad2dc9da32fc73772e9867c18
Serhii Pererva <serhii.pererva@sky.uk>
2025-03-24 14:31:49 +0000

    initial

---
+++ build.sbt
@@ -0,0 +1,16 @@
+ThisBuild / organization := "com.example"
+ThisBuild / scalaVersion := "3.6.2"
+
+lazy val root = (project in file(".")).settings(
+  name := "rockpaperscissors",
+  libraryDependencies ++= Seq(
+    // "core" module - IO, IOApp, schedulers
+    // This pulls in the kernel and std modules automatically.
+    "org.typelevel" %% "cats-effect" % "3.3.12",
+    // concurrency abstractions and primitives (Concurrent, Sync, Async etc.)
+    "org.typelevel" %% "cats-effect-kernel" % "3.3.12",
+    // standard "effect" library (Queues, Console, Random etc.)
+    "org.typelevel" %% "cats-effect-std" % "3.3.12",
+    "org.typelevel" %% "cats-effect-testing-specs2" % "1.4.0" % Test
+  )
+)
---
+++ src/main/scala/com/rps/Main.scala
@@ -0,0 +1,172 @@
+package com.rps
+
+import cats.effect.IOApp
+import cats.effect.IO
+import cats.effect.std.Console
+import cats.Monad
+import cats.syntax.flatMap._
+import cats.syntax.functor._
+import cats.syntax.applicative._
+import cats.syntax.either._
+import cats.Show
+import cats.syntax.contravariant._
+import cats.syntax.show._
+import javax.sound.sampled.Clip
+import cats.Applicative
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+enum Move:
+  case Rock, Paper, Scissors
+
+object Move {
+  enum ReadError:
+    case InvalidMove(input: String)
+
+  def fromString(in: String): Either[ReadError, Move] = in match {
+    case "rock" | "r"     => Rock.asRight[ReadError]
+    case "paper" | "p"    => Paper.asRight[ReadError]
+    case "scissors" | "s" => Scissors.asRight[ReadError]
+    case other            => ReadError.InvalidMove(other).asLeft[Move]
+  }
+
+  given Show[ReadError] = Show[String].contramap[ReadError] {
+    case ReadError.InvalidMove(input) =>
+      s"Specified input $input is not valid input. Valid inputs are: rock, paper, scissors (r, p, s)"
+  }
+}
+
+enum Result:
+  case Win[F[_]](winner: Player[F])
+  case Tie
+
+object Result {
+
+  implicit val showForResult: Show[Result] = Show[String].contramap[Result] {
+    case Win(p) => s"${p.name} won this round"
+    case Tie    => s"There was a tie!"
+  }
+}
+
+sealed abstract trait Player[F[_]] {
+
+  val name: String
+
+  def getMove(): F[Move]
+}
+
+sealed abstract trait RockPaperScissors[F[_]] {
+  def round(player1: Player[F], player2: Player[F]): F[Result]
+}
+
+final class CliPlayer[F[_]: Console: Monad](val name: String)
+    extends Player[F] {
+
+  override def getMove(): F[Move] = for {
+    _      <- Console[F].println(s"Your next move $name: ")
+    move   <- Console[F].readLine.map(Move.fromString)
+    result <-
+      move.fold(error => Console[F].println(error.show) >> getMove(), _.pure[F])
+  } yield result
+
+}
+
+final class CliRockPaperScissors[F[_]: Console: Monad]
+    extends RockPaperScissors[F] {
+
+  override def round(player1: Player[F], player2: Player[F]): F[Result] = for {
+    _         <- Console[F].println("The round has started")
+    choiceOne <- player1.getMove()
+    choiceTwo <- player2.getMove()
+    _         <- Console[F].println(s"Player ${player1.name} choses $choiceOne")
+    _         <- Console[F].println(s"Player ${player2.name} choses $choiceTwo")
+
+    result = (choiceOne, choiceTwo) match {
+               case Move.Rock -> Move.Rock         => Result.Tie
+               case Move.Scissors -> Move.Scissors => Result.Tie
+               case Move.Paper -> Move.Paper       => Result.Tie
+               case Move.Rock -> Move.Scissors     => Result.Win(player1)
+               case Move.Scissors -> Move.Paper    => Result.Win(player1)
+               case Move.Paper -> Move.Rock        => Result.Win(player1)
+               case Move.Scissors -> Move.Rock     => Result.Win(player2)
+               case Move.Paper -> Move.Scissors    => Result.Win(player2)
+               case Move.Rock -> Move.Paper        => Result.Win(player2)
+             }
+  } yield result
+}
+
+object Main extends IOApp.Simple {
+
+  def run: IO[Unit] = {
+    val player1 = new CliPlayer[IO]("Bob")
+    val player2 = new CliPlayer[IO]("Alice")
+    val game    = new CliRockPaperScissors[IO]
+
+    game.round(player1, player2).map(_.show) >>= IO.println
+  }
+}
