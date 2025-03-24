d313b1c9d12052f64dd28b75b92a58e4fb9a5fb6
Serhii Pererva <serhii.pererva@gmail.com>
2025-03-24 15:32:17 +0000

    Shitty one

--- src/main/scala/com/rps/Main.scala
+++ src/main/scala/com/rps/Main.scala
@@ -4,15 +4,11 @@ import cats.effect.IOApp
 import cats.effect.IO
 import cats.effect.std.Console
 import cats.Monad
-import cats.syntax.flatMap._
-import cats.syntax.functor._
-import cats.syntax.applicative._
-import cats.syntax.either._
+import cats.syntax.all.*
 import cats.Show
-import cats.syntax.contravariant._
-import cats.syntax.show._
 import javax.sound.sampled.Clip
 import cats.Applicative
+import cats.Functor
 
 enum Move:
   case Rock, Paper, Scissors
@@ -28,9 +24,8 @@ object Move {
     case other            => ReadError.InvalidMove(other).asLeft[Move]
   }
 
-  given Show[ReadError] = Show[String].contramap[ReadError] {
-    case ReadError.InvalidMove(input) =>
-      s"Specified input $input is not valid input. Valid inputs are: rock, paper, scissors (r, p, s)"
+  given Show[ReadError] = Show[String].contramap[ReadError] { case ReadError.InvalidMove(input) =>
+    s"Specified input $input is not valid input. Valid inputs are: rock, paper, scissors (r, p, s)"
   }
 }
 
@@ -57,8 +52,7 @@ sealed abstract trait RockPaperScissors[F[_]] {
   def round(player1: Player[F], player2: Player[F]): F[Result]
 }
 
-final class CliPlayer[F[_]: Console: Monad](val name: String)
-    extends Player[F] {
+final class CliPlayer[F[_]: Console: Monad](val name: String) extends Player[F] {
 
   override def getMove(): F[Move] = for {
     _      <- Console[F].println(s"Your next move $name: ")
@@ -69,15 +63,11 @@ final class CliPlayer[F[_]: Console: Monad](val name: String)
 
 }
 
-final class CliRockPaperScissors[F[_]: Console: Monad]
-    extends RockPaperScissors[F] {
+final class CliRockPaperScissors[F[_]: Console: Monad] extends RockPaperScissors[F] {
 
   override def round(player1: Player[F], player2: Player[F]): F[Result] = for {
-    _         <- Console[F].println("The round has started")
     choiceOne <- player1.getMove()
     choiceTwo <- player2.getMove()
-    _         <- Console[F].println(s"Player ${player1.name} choses $choiceOne")
-    _         <- Console[F].println(s"Player ${player2.name} choses $choiceTwo")
 
     result = (choiceOne, choiceTwo) match {
                case Move.Rock -> Move.Rock         => Result.Tie
@@ -93,13 +83,48 @@ final class CliRockPaperScissors[F[_]: Console: Monad]
   } yield result
 }
 
+final class BestOfNGame[F[_]: Monad: Console](baseGame: RockPaperScissors[F], totalGames: Int) {
+  var results = List.empty[Result]
+
+  def rounds(player1: Player[F], player2: Player[F]): F[Result] = {
+    baseGame
+      .round(player1: Player[F], player2: Player[F])
+      .replicateA(totalGames)
+      .map(games => results = games)
+      .flatMap(_ => printRounds)
+      .map(_ => computeResult(player1, player2))
+  }
+
+  private def computeResult(player1: Player[F], player2: Player[F]): Result = {
+    var player1WinCount = 0
+    var player2WinCount = 0
+    results foreach {
+      case Result.Win(p) => if p == player1 then player1WinCount += 1 else player2WinCount += 1
+      case Result.Tie    => ()
+    }
+    if player1WinCount > player2WinCount then Result.Win(player1)
+    else if player2WinCount > player1WinCount then Result.Win(player2)
+    else Result.Tie
+  }
+
+  private def printRounds(using Console[F]): F[Unit] = {
+    results.zipWithIndex.traverse_ { case (result, index) =>
+      Console[F].println(s"Round ${index + 1}: ${result.show}")
+    }
+  }
+}
+
 object Main extends IOApp.Simple {
 
   def run: IO[Unit] = {
-    val player1 = new CliPlayer[IO]("Bob")
-    val player2 = new CliPlayer[IO]("Alice")
-    val game    = new CliRockPaperScissors[IO]
+    val player1  = new CliPlayer[IO]("Bob")
+    val player2  = new CliPlayer[IO]("Alice")
+    val baseGame = new CliRockPaperScissors[IO]
+    val game     = new BestOfNGame(baseGame, totalGames = 3)
 
-    game.round(player1, player2).map(_.show) >>= IO.println
+    for {
+      result <- game.rounds(player1, player2)
+      _      <- IO.println(result.show)
+    } yield ()
   }
 }
